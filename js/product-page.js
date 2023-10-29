2fcc970d1adcbca826371217bca0ba921ba51740
Veronica <ve.ro97@hotmail.com>
2023-10-29 23:38:14 +0100

    api

--- js/product-page.js
+++ js/product-page.js
@@ -1,33 +1,34 @@
 document.addEventListener("DOMContentLoaded", async () => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const id = urlParams.get("id");
-	const gameTitle = document.getElementById("game-title");
+	const gameName = document.getElementById("game-title");
 	const gameDetails = document.getElementById("game-details");
 
 	if (!id) {
-		gameTitle.innerHTML = "Game/Product ID not provided";
+		gameName.innerHTML = "Game/Product ID not provided";
 		return;
 	}
 
 	let results;
+	let response;
 	try {
-		const response = await fetch(`https://api.noroff.dev/api/v1/gamehub/${id}`);
+		response = await fetch(`https://www.fadnes.me/wp-json/wc/store/products/${id}`);
 		results = await response.json();
 	} catch (e) {
-		gameTitle.innerHTML = "Game/Product does not exist";
+		gameName.innerHTML = "Game/Product does not exist";
 		return;
 	}
 
-	if (results.statusCode && results.statusCode !== 200) {
-		gameTitle.innerHTML = "Game/Product does not exist";
+	if (response.status && response.status !== 200) {
+		gameName.innerHTML = "Game/Product does not exist";
 		return;
 	}
 
-	gameTitle.innerHTML = results.title;
+	gameName.innerHTML = results.name;
 
 	const gameImage = document.getElementById("game-image");
-	gameImage.src = results.image;
-	gameImage.alt = `${results.title} Game Image`;
+	gameImage.src = results.images[0].src;
+	gameImage.alt = `${results.name} Game Image`;
 
 	const gameText = document.getElementById("game-text");
 	gameText.innerHTML = results.description;
