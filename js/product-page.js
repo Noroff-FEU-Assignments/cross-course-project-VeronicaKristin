5558c04cadc68b36b862cade0853269151a7db22
Veronica <ve.ro97@hotmail.com>
2023-12-12 16:01:15 +0100

    swaped api

--- js/product-page.js
+++ js/product-page.js
@@ -1,34 +1,33 @@
 document.addEventListener("DOMContentLoaded", async () => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const id = urlParams.get("id");
-	const gameName = document.getElementById("game-title");
+	const gameTitle = document.getElementById("game-title");
 	const gameDetails = document.getElementById("game-details");
 
 	if (!id) {
-		gameName.innerHTML = "Game/Product ID not provided";
+		gameTitle.innerHTML = "Game/Product ID not provided";
 		return;
 	}
 
 	let results;
-	let response;
 	try {
-		response = await fetch(`https://www.fadnes.me/wp-json/wc/store/products/${id}`);
+		const response = await fetch(`https://api.noroff.dev/api/v1/gamehub/${id}`);
 		results = await response.json();
 	} catch (e) {
-		gameName.innerHTML = "Game/Product does not exist";
+		gameTitle.innerHTML = "Game/Product does not exist";
 		return;
 	}
 
-	if (response.status && response.status !== 200) {
-		gameName.innerHTML = "Game/Product does not exist";
+	if (results.statusCode && results.statusCode !== 200) {
+		gameTitle.innerHTML = "Game/Product does not exist";
 		return;
 	}
 
-	gameName.innerHTML = results.name;
+	gameTitle.innerHTML = results.title;
 
 	const gameImage = document.getElementById("game-image");
-	gameImage.src = results.images[0].src;
-	gameImage.alt = `${results.name} Game Image`;
+	gameImage.src = results.image;
+	gameImage.alt = `${results.title} Game Image`;
 
 	const gameText = document.getElementById("game-text");
 	gameText.innerHTML = results.description;
--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	};
 
 	try {
-		const response = await fetch("https://www.fadnes.me/wp-json/wc/store/products");
+		const response = await fetch("https://api.noroff.dev/api/v1/gamehub");
 		results = await response.json();
 	} catch (e) {
 		handleError();
@@ -27,12 +27,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 	for (const item of results) {
 		const product = Object.assign(document.createElement("div"), { id: item.id, className: "game-item border" });
 
-		product.appendChild(Object.assign(document.createElement("img"), { src: item.images[0].src, className: "all-games_photos", alt: item.name }));
+		product.appendChild(Object.assign(document.createElement("img"), { src: item.image, className: "all-games_photos", alt: item.title }));
 
 		product.appendChild(
 			Object.assign(document.createElement("a"), {
 				href: `/products/game.html?id=${item.id}`,
-				innerHTML: `$${item.prices.regular_price}`,
+				innerHTML: `$${item.price}`,
 				style: "background: white",
 			})
 		);
@@ -42,16 +42,16 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 	productList.removeChild(productList.getElementsByClassName("lds-dual-ring")[0]);
 
-	const on_sale = results.filter((item) => item.on_sale);
-	for (const item of on_sale) {
+	const onSale = results.filter((item) => item.onSale);
+	for (const item of onSale) {
 		const product = Object.assign(document.createElement("div"), { id: item.id, className: "top-seller-item" });
 
-		product.appendChild(Object.assign(document.createElement("img"), { src: item.images[0].src, alt: item.title }));
+		product.appendChild(Object.assign(document.createElement("img"), { src: item.image, alt: item.title }));
 
 		product.appendChild(
 			Object.assign(document.createElement("a"), {
 				href: `/products/game.html?id=${item.id}`,
-				innerHTML: `<s>$${item.prices.regular_price}</s> $${item.prices.sale_price}`,
+				innerHTML: `<s>$${item.price}</s> $${item.discountedPrice}`,
 				className: "top-seller-button",
 				style: "background: white",
 			})
